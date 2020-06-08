950cb0f76c400d131d26912143244f88e4ef8252
Anshul Chhabra <anshul.chhabra@zensar.com>
2020-06-08 19:05:11 +0530

    app pushed

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public nameofurl:string;
+  public url:string;
+  constructor(private router: Router) {}
 
-  constructor() {}
-
+  uploadURL(){
+    this.url=this.nameofurl;
+    localStorage.setItem('url', this.url);
+    this.router.navigateByUrl('/result');
+  }
 }
