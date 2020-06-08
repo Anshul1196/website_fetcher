c0563cebc7f0a175348f73cbbee9db2673237220
Anshul Chhabra <anshul.chhabra@zensar.com>
2020-06-08 19:34:36 +0530

    adding files

---
+++ src/app/result/result.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DomSanitizer} from '@angular/platform-browser';
+@Component({
+  selector: 'app-result',
+  templateUrl: './result.page.html',
+  styleUrls: ['./result.page.scss'],
+})
+export class ResultPage implements OnInit {
+
+  constructor(private router: Router,public sanitizer: DomSanitizer) { }
+
+  ngOnInit() {
+  }
+  goback(){
+    this.router.navigateByUrl('/home');
+  }
+  openURL(){
+    
+    return this.sanitizer.bypassSecurityTrustResourceUrl(localStorage.getItem("url"));
+  
+  }
+}
